e2706bde642e3299b850c455cdcc86ce59d9e20a
Philip Lau <cnlau.philip@outlook.com>
2026-03-16 17:47:08 +0800

    Update docanax.rb
    - update sha256

--- docanax.rb
+++ docanax.rb
@@ -2,7 +2,7 @@ class Docanax < Formula
   desc "High-performance CLI tool for document extraction (PDF, MS Docs)"
   homepage "https://github.com/thinkwider-co/docanax"
   url "https://github.com/thinkwider-co/docanax/releases/download/v1.0.1/docanax-macos-arm64.zip"
-  sha256 "636d11d53bb3d7b49d099c7a7e0c0f05b06e3abb5c329ada96a2f273e508b1b0"
+  sha256 "53d248881e5bd6fa757ee456c586046056322bd6125280977a577263c5ed3d41"
   version "1.0.1"
 
   def install
